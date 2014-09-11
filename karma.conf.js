9334aa861dede51afff0293653d2ad30410f280f
fpellet <florent.pellet@gmail.com>
2014-09-11 17:13:37 +0200

    add jquery libs

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
 	config.set({
 		basePath: '',
 		frameworks: ['mocha', 'chai'],
-		files: ['src/**/*.js', 'tests/**/*.js'],
+		files: ['src/**/*.js', 'tests/**/*.js', 'libs/*.js'],
 		reporters: ['progress'],
 		autoWatch: true,
 		browsers: ['PhantomJS'],
