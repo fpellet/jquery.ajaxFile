5da22742b0d9c6e796a9e62bc6520b388d99a030
fpellet <florent.pellet@gmail.com>
2014-09-11 17:33:57 +0200

    Add option

---
+++ src/Option.ts
@@ -0,0 +1,41 @@
+﻿/// <reference path="../libs/jquery.d.ts" />
+/// <reference path="url.ts" />
+
+interface IFileData {
+    name: string;
+    element: HTMLInputElement;
+}
+
+interface IAjaxFileOption {
+    type?: string;
+    url?: string;
+
+    data?: any;
+    files?: IFileData[];
+    dataType?: string;
+
+    timeout?: number;
+
+    error? (jqXHR: JQueryXHR, textStatus: string, errorThrown: string): any;
+    success? (data: any, textStatus: string, jqXHR: JQueryXHR): any;
+    complete? (jqXHR: JQueryXHR, textStatus: string): any;
+
+    global?: boolean;
+}
+
+var defaultOption: IAjaxFileOption = {
+    data: {},
+    files: [],
+    dataType: "json",
+    type: 'POST'
+};
+
+var mergeWithDefaultOption = (option: IAjaxFileOption): IAjaxFileOption => {
+    option = $.extend(true, {}, $.ajaxSettings, defaultOption, option);
+
+    if (!option.url) {
+        option.url = getCurrentUrlWithoutHash();
+    }
+
+    return option;
+};
---
+++ tests/OptionTest.ts
@@ -0,0 +1,52 @@
+﻿/// <reference path="../libs/chai.d.ts" />
+/// <reference path="../src/option.ts" />
+/// <reference path="../libs/mocha.d.ts" />
+
+var expect = chai.expect;
+var assert = chai.assert;
+
+describe("Option", () => {
+    it("When mergeWithDefaultOption with empty option Then url is current url without hash", () => {
+        window.location.href += '#hello';
+
+        var result = mergeWithDefaultOption({});
+
+        expect(result.url).to.equals(getCurrentUrlWithoutHash());
+    });
+
+    it("When mergeWithDefaultOption with url Then url is not override", () => {
+        var url = 'http://example.com';
+
+        var result = mergeWithDefaultOption({ url: url});
+
+        expect(result.url).to.equals(url);
+    });
+
+    it("When mergeWithDefaultOption with empty option Then use default option", () => {
+        var result = mergeWithDefaultOption({});
+
+        expect(result.data).to.eqls({});
+        expect(result.files).to.eqls([]);
+        expect(result.dataType).to.eqls('json');
+        expect(result.type).to.eqls('POST');
+    });
+
+    it("When mergeWithDefaultOption with empty option Then use jquery default option", () => {
+        $.ajaxSettings.timeout = 5;
+
+        var result = mergeWithDefaultOption({});
+
+        expect(result.global).to.eqls(true);
+        expect(result.timeout).to.eqls(5);
+    });
+
+    it("When mergeWithDefaultOption Then use default parameter in user option, then in default option, then in jquery option", () => {
+        $.ajaxSettings.timeout = 5;
+        defaultOption.timeout = 6;
+
+        var result = mergeWithDefaultOption({ dataType: 'xml' });
+
+        expect(result.timeout).to.eqls(6);
+        expect(result.dataType).to.eqls('xml');
+    });
+}); 
