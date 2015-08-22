af7e9dda5893442c815713d13665d38418982f01
fpellet <florent.pellet@gmail.com>
2015-08-23 01:24:02 +0200

    Use jquery of npm

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
 	config.set({
 		basePath: '',
 		frameworks: ['mocha', 'chai', 'source-map-support'],
-		files: ['libs/*.js', 'tests/merge.js'],
+		files: ['node_modules/jquery/dist/jquery.js', 'tests/merge.js'],
 		reporters: ['progress'],
 		autoWatch: true,
 		browsers: ['PhantomJS'],
