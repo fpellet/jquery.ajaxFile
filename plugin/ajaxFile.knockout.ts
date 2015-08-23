c15595202784bae4db2a8e4ded86cf1ec5df2832
fpellet <florent.pellet@gmail.com>
2015-08-23 16:04:10 +0200

    Add plugin for knockout

---
+++ plugin/ajaxFile.knockout.ts
@@ -0,0 +1,38 @@
+﻿namespace AjaxFile.Knockout {
+    class FileInputWrapper {
+        constructor(private input: HTMLInputElement) {
+        }
+
+        public getElement(): HTMLInputElement {
+            return this.input;
+        }
+
+        public fileSelected(): boolean {
+            return !!this.input.value;
+        }
+    }
+
+    export function registerBindingHandler(ko: KnockoutStatic, $: JQueryStatic): void {
+        ko.bindingHandlers["file"] = {
+            init(element, valueAccessor) {
+                $(element).change(function () {
+                    const value = valueAccessor();
+                    if (this.value) {
+                        value(new FileInputWrapper(this));
+                    } else {
+                        value(undefined);
+                    }
+                });
+            },
+            update(element, valueAccessor) {
+                const value = valueAccessor();
+                if (!ko.unwrap(value) && element.value) {
+                    const $element = $(element);
+                    $element.replaceWith($element.clone(true, true));
+                }
+            }
+        };
+    }
+}
+
+AjaxFile.Knockout.registerBindingHandler(ko, $);
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="chai/chai.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="mocha/mocha.d.ts" />
+/// <reference path="knockout/knockout.d.ts" />
